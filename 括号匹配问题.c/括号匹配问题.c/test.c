3ee4190a4cc418e470817dbba3bbff7803ef3cc6
ynlyxy <2021517018@qq.com>
2019-11-13 21:59:24 +0800

    （（）（））去掉包裹括号

---
+++ 括号匹配问题.c/括号匹配问题.c/test.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+char * removeOuterParentheses(char * S)
+{
+	int i, sum = 0, j = 0;
+	int len = strlen(S);
+	for (i = 0; i<len; i++)
+	{
+		if (S[i] == '(')
+		{
+			sum += 1;
+		}
+		else
+		{
+			sum -= 1;
+		}
+		if ((S[i] == '(') && (sum>1))
+		{
+			S[j] = '(';
+			j++;
+		}
+		if (S[i] == ')' && sum > 0)
+		{
+			S[j] = ')';
+			j++;
+		}
+	}
+	S[j] = '\0';
+	return S;
+}
+int main()
+{
+	char S[] = "(()())()";
+	//printf("%s", S);
+	char *s = removeOuterParentheses(S);
+	printf("%s", s);
+	system("pause");
+	return 0;
+}
